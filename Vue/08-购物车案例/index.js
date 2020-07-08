14cd997875de13e05da5e62c27eb71a903500ad9
TianchiWu <839385081@qq.com>
2020-07-08 17:34:19 +0800

    Day 4

---
+++ Vue/08-购物车案例/index.js
@@ -0,0 +1,64 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        shoppingList: [
+            {
+                id: 1,
+                name: '《算法导论》',
+                date: '2006-9',
+                price: 85.00,
+                counter: 1
+            },
+            {
+                id: 2,
+                name: '《UNIX编程基础》',
+                date: '2006-2',
+                price: 59.00,
+                counter: 1
+            },
+            {
+                id: 3,
+                name: '《Head First Java》',
+                date: '2008-10',
+                price: 87.00,
+                counter:1
+            },
+            {
+                id: 4,
+                name: '《C++ premier plus》',
+                date: '2009-1',
+                price: 143.00,
+                counter: 1
+            }
+
+        ]
+    },
+    methods: {
+        decrement(index){
+            this.shoppingList[index].counter--
+            //console.log('decrement')
+        },
+        increment(index){
+            this.shoppingList[index].counter++
+            //console.log('increment')
+        },
+        removeHandler(index){
+            this.shoppingList.splice(index,1)
+        }
+    },
+    filters: {
+        showPrice(price){
+            return '￥'+price.toFixed(2)
+        }
+    },
+    computed: {
+        totalPrice(){
+            let totalPrice=0;
+            // for(let item of this.shoppingList){
+            //     totalPrice+=item.counter*item.price
+            // }
+
+            return totalPrice
+        }
+    }
+})
---
+++ Vue/README.md
@@ -0,0 +1,31 @@
+## MVVM
+Vue对应DOM，Model对应JS里的数据。绑定数据
+
+## 传入Vue的options
+##### el: string| HTMLElements
+##### data: Object| Function(组件中必须是函数)
+##### methods: Function
+
+## Vue的生命周期
+index.js Vue的入口
+
+##### create: function(){}
+##### mounted: function(){}
+都是在Vue里有完整生命周期的函数
+
+## 插值操作
+v-text 纯文本
+v-pre 不解析
+v-cloak 解析之前，遮挡文本
+
+##### v-bind 动态绑定属性
+
+## v-on
+##### @click语法糖 @click="handlesubmit"
+##### 如果方法不需要额外参数，方法后的（）可以不添加，但是如果方法本身有一个参数，会默认将原生事件event参数传递进去
+##### 如果需要传入某个参数，同时需要event，可以通过$event传入事件
+
+##JS高阶函数
+##### filter,map,reduce
+##### columns = [10,20,30,40,120]
+##### columns.filter()

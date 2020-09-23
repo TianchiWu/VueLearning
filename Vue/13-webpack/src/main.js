76431b42eeba26a6cc990ff279686bbe5d28ef63
Jack_Woo <wtc20000229@gmail.com>
2020-09-23 21:16:06 +0800

    0923

---
+++ Vue/13-webpack/src/main.js
@@ -0,0 +1,10 @@
+const {add, sub} = require('./js/mathUtils')
+
+console.log(add(20,30))
+console.log(sub(20,30))
+
+//依赖css文件
+require('./css/normal.css')
+require('./css/special.less')
+
+document.writeln('<h2>标题</h2>')
---
+++ Vue/13-webpack/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        //动态路径
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: 'dist/'
+    },
+    module: {
+        rules: [
+            //style添加到dom中
+            //从右向左读
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader'
+            },
+            //匹配less文件
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
+            },
+            //匹配图片文件
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            //加载的图片小于limit显示，否则以file形式显示
+                            limit: 100000,
+                            //打包加入信息
+                            name: 'img/[name].[hash:8].[ext]'
+                        },
+                    }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
+            },
+        ]
+    }
+}
